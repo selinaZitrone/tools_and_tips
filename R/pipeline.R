695131b11bff022fba2033ca570b6a3937576c2b
Selina Baldauf <selina.baldauf@gmx.de>
2023-06-15 18:20:52 +0200

    Final version git slides

--- R/pipeline.R
+++ R/pipeline.R
@@ -12,10 +12,6 @@ lapply(slides_html, function(x) {
   )
 })
 
-# copy penguin pdf to docs
-file.copy(from = "sessions/additional_material/02_quarto/penguins_paper.pdf",
-          to = "docs/sessions/additional_material/")
-
 # commit and push
 git2r::add(path = here::here("docs"))
 git2r::commit(message = "re-render site")
