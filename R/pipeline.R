fd653b1259879fb9a8d0b9bd750b7607fb3b3b60
Selina Baldauf <selina.baldauf@fu-berlin.de>
2025-05-13 23:30:51 +0200

    Auto-format code

--- R/pipeline.R
+++ R/pipeline.R
@@ -2,14 +2,14 @@
 quarto::quarto_render()
 
 # render slides as pdf
-slides_html <- list.files(here::here("docs/slides/"),
-  pattern = "*.html", full.names = TRUE
+slides_html <- list.files(
+  here::here("docs/slides/"),
+  pattern = "*.html",
+  full.names = TRUE
 )
 
 lapply(slides_html, function(x) {
-  pagedown::chrome_print(x,
-    format = "pdf"
-  )
+  pagedown::chrome_print(x, format = "pdf")
   print(paste0("Printed ", x))
 })
 
